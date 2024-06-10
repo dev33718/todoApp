8e4975d5683078e5651bdda643fffb6e7dcf6825
dev33718 <85031212+dev33718@users.noreply.github.com>
2024-06-10 17:18:22 +0530

    Update authMiddleware.js

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -2,16 +2,13 @@ const jwt = require("jsonwebtoken");
 const jwtSecret = process.env.JWT_SECRET;
 
 const authMiddleware = (req, res, next) => {
-  // Get token from header
   const token = req.header("x-auth-token");
 
-  // Check if not token
   if (!token) {
     return res.status(401).json({ error: "No token, authorization denied" });
   }
 
   try {
-    // Verify token
     const decoded = jwt.verify(token, jwtSecret);
     req.user = decoded.user;
     next();
